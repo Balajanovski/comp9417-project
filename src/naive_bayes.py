da939523fc7697a47ea95d3208281f82276c4765
Zhizhou Ma <zm9913@gmail.com>
2021-07-01 11:35:48 +0930

    refactoring code to make use of sklearn CountVectorizer

---
+++ src/naive_bayes.py
@@ -0,0 +1,33 @@
+import src.util as util
+from sklearn.naive_bayes import BernoulliNB, MultinomialNB
+from time import time
+import tqdm
+from src.print_metrics import print_metrics
+import sys
+
+def run_naive_bayes(is_bernouli: bool, min_ngram: int, max_ngram: int):
+    st = time()
+
+    X_train, X_test, y_train, y_test = util.load_data(is_bernouli, min_ngram, max_ngram)
+
+    model = BernoulliNB() if is_bernouli else MultinomialNB()
+
+    print("Fitting model")
+    model.fit(X_train,y_train)
+
+    y_pred = model.predict(X_test)
+    print_metrics(y_pred, y_test)
+
+    print(f"Time: {time()-st}s")
+
+def main():
+    args = sys.argv
+
+    if args[1] != "bernouli" and args[1] != "multinomial":
+        raise "invalid input, argument 1 must be either 'bernouli' or 'multinomial'"
+    
+    print(f"Starting {args[1]}")
+    run_naive_bayes(args[1] == "bernouli", int(args[2]), int(args[3]))
+
+if __name__ == "__main__":
+    main()
--- src/util.py
+++ src/util.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from processed_data.processed_data_folder import PROCESSED_DATA_FOLDER_PATH
 import os
-import string
+from typing import Dict, List, Tuple
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 def load_word_map():
     word_map_df = pd.read_csv(os.path.join(PROCESSED_DATA_FOLDER_PATH, "word_map.csv"), dtype="string", keep_default_na=False, na_filter=False)
@@ -12,8 +15,19 @@ def load_word_map():
     
     return word_map
 
+def create_bags_of_words(train_data: List[str], test_data: List[str], is_binary: bool, min_ngram: int, max_ngram: int) -> Tuple[np.array, np.array]:
+    vectorizer = CountVectorizer(token_pattern=r"[^\s]+", binary=is_binary, ngram_range=(min_ngram, max_ngram))
+    X_train = vectorizer.fit_transform(train_data)
+    X_test = vectorizer.transform(test_data)
+    return X_train, X_test
 
-def load_train():
+def load_data(is_binary: bool = True, min_ngram: int = 1, max_ngram: int = 1) -> Tuple[np.array, np.array, np.array, np.array]:
     df = pd.read_csv(os.path.join(PROCESSED_DATA_FOLDER_PATH, "processed_train.csv"))
     df["question_text"] = df["question_text"].apply(str)
-    return df["question_text"], df["target"]
+    X, y = df["question_text"], df["target"]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=8)
+
+    X_train, X_test = create_bags_of_words(X_train, X_test, is_binary, min_ngram, max_ngram)
+
+    return X_train, X_test, y_train, y_test
